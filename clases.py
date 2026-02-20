b8b7a819f1cfab5b89d66432e7bdf59dadcbce12
Christopher Figueroa <christopherfigueroa087@gmail.com>
2026-02-20 16:54:58 -0600

    Aqui esta esto otro

--- clases.py
+++ clases.py
@@ -4,3 +4,22 @@ class Usuario():
         self.apellidos=apellidos
         self.dui=DUI
         self.password=password
+
+class Administrador(Usuario):
+    def __init__(self, nombres, apellidos, DUI, password, userName):
+        super().__init__(nombres, apellidos, DUI, password, userName)
+
+class Cliente(Usuario):
+    def __init__(self, nombres, apellidos, DUI, password, userName):
+        super().__init__(nombres, apellidos, DUI, password, userName)
+
+class CuentaBancaria():
+    def __init__(self,propietario,tipo,saldo,estado):
+        self.propietario=propietario
+        self.tipo=tipo
+        self.saldo=saldo
+        self.estado=estado
+        pass
+
+
+#Aqui tengo la duda si se hace una clase tanto para ceunta de ahorro como corriente
