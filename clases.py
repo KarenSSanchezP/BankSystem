3d9586935ae9f314007c6a302a1b16776c44a929
Christopher Figueroa <christopherfigueroa087@gmail.com>
2026-02-20 16:00:27 -0600

    Aqui comence a adelantar compañeros lo que es las clases

---
+++ clases.py
@@ -0,0 +1,6 @@
+class Usuario():
+    def __init__(self,nombres,apellidos,DUI,password,userName):
+        self.nombres=nombres
+        self.apellidos=apellidos
+        self.dui=DUI
+        self.password=password
