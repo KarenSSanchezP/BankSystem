69159fe3c28da9da367cea0ad6a869e65bbb181b
Christopher Figueroa <christopherfigueroa087@gmail.com>
2026-02-21 14:24:37 -0600

    Otros cambios

--- banksystemapp/src/models/usuarios/usuario.py
+++ banksystemapp/src/models/usuarios/usuario.py
@@ -2,54 +2,56 @@ from abc import ABC, abstractmethod
 
 class Usuario(ABC):
   def __init__(self,userId,nombres,apellidos,dui,password,rol,userName=None):
-    self._userId=userId
-    self._nombres=nombres
-    self._apellidos=apellidos
-    self._dui=dui
-    self._password=password
-    self._rol=rol
-    self._userName=userName
+        self._userId=userId
+        self._nombres=nombres
+        self._apellidos=apellidos
+        self._dui=dui
+        self._password=password
+        self._rol=rol
+        self._userName=userName
     
-    if userName:
-      self._userName=userName
-    else:
-      self._userName=self.crearUserName()
+        if userName:
+            self._userName=userName
+        else:
+            self._userName=self.crearUserName()
 
-    @property
-    def userId(self):
-      return self.userId
+        @property
+        def userId(self):
+            return self._userId
         
-    @property
-    def nombres(self):
-      return self._nombres
+        @property
+        def nombres(self):
+            return self._nombres
+      
+        @property
+        def apellidos(self):
+            return self._apellidos
     
-    
-    @property
-    def apellidos(self):
-      return self._apellidos
-    
-    @property
-    def dui(self):
-      return self._dui
-    
-    @property
-    def password(self):
-      return self._password
-    
-    @property
-    def rol(self):
-      return self._rol
-    
+        @property
+        def dui(self):
+            return self._dui
+        
+        def verificarPassword(self,password):
+            return self._password == password
 
-    def crearUserName(self):
-      nombre1=self.nombre
-      apellido1=self.apellido
-      nombre=(nombre1+apellido1).split('')
-      nombre1=[0]
-      apellido1=[0]
-      return f"{nombre1}{apellido1}{self.userId}"
+        @property
+        def password(self):
+            return self._password
     
-    @property
-    def userName(self):
-      return self.userName
+        @property
+        def rol(self):
+            return self._rol
     
+        @abstractmethod
+        def crearUserName(self):
+            nombre1=self._nombre
+            apellido1=self._apellido
+            nombre=(nombre1+apellido1).split('')
+            nombre1=[0]
+            apellido1=[0]
+            return f"{nombre1}{apellido1}{self.userId}"
+    
+        @property
+        def userName(self):
+            return self._userName
+            
